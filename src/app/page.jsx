bf33e5f8dbc647051a8014bb15b09e4ccecf634e
audiph <lagarde.marcjoseph@gmail.com>
2024-03-16 18:59:10 +0800

    integrate page transition

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const aboutMe = {
   title: 'Hello! This is Jeff!',
@@ -8,36 +11,43 @@ const aboutMe = {
 
 const HomePage = () => {
   return (
-    <div className="h-full flex flex-col lg:flex-row px-4 gap-8 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      {/* IMAGE CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-        <Image
-          src="/hero.png"
-          alt="JL's Picture"
-          fill
-          priority
-          className="object-contain drop-shadow-xl"
-        />
-      </div>
-      {/* TEXT CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
-        {/* TITLE */}
-        <h1 className="text-4xl md:text-6xl font-bold text-[#ECEFF1]">
-          {aboutMe.title}
-        </h1>
-        {/* DESCRIPTION */}
-        <p className="text-[#B0BEC5] md:text-xl">{aboutMe.description}</p>
-        {/* BUTTONS */}
-        <div className="w-full flex gap-4">
-          <button className="p-4 rounded-lg ring-1 ring-[#ECEFF1] bg-[#ECEFF1] text-[#222222]">
-            View My Work
-          </button>
-          <button className="p-4 rounded-lg ring-1 ring-[#ECEFF1] text-[#ECEFF1]">
-            Contact Me
-          </button>
+    <motion.div
+      className="h-full"
+      initial={{ y: '-200vh' }}
+      animate={{ y: '0%' }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 gap-8 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image
+            src="/hero.png"
+            alt="JL's Picture"
+            fill
+            priority
+            className="object-contain drop-shadow-xl"
+          />
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* TITLE */}
+          <h1 className="text-4xl md:text-6xl font-bold text-[#ECEFF1]">
+            {aboutMe.title}
+          </h1>
+          {/* DESCRIPTION */}
+          <p className="text-[#B0BEC5] md:text-xl">{aboutMe.description}</p>
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-[#ECEFF1] bg-[#ECEFF1] text-[#222222]">
+              View My Work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-[#ECEFF1] text-[#ECEFF1]">
+              Contact Me
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
