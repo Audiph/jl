be0ffb3b73c3532acc61e77fef3313afc24c1c3c
audiph <lagarde.marcjoseph@gmail.com>
2024-03-16 12:39:03 +0800

    navbar and layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,23 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Jeff Lagarde',
-  description: 'My Web Portfolio',
+  title: 'JL',
+  description: "JL's Dev Portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="w-screen h-screen bg-red-500">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
